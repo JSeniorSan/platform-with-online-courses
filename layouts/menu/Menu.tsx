40fb61dfbe447d7342505762a6cfa9f873f93741
Tekai_ <vip007_007@mail.ru>
2023-09-30 16:29:58 +0300

    Active menu list

--- layouts/menu/Menu.tsx
+++ layouts/menu/Menu.tsx
@@ -14,6 +14,8 @@ import BooksIcon from "./icons/Books";
 import ProductsIcon from "./icons/Products";
 import styles from "./menu.module.scss";
 import cn from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 const firstLevelMenu: IFirstLevelMenuItem[] = [
   {
     route: "courses",
@@ -42,15 +44,27 @@ const firstLevelMenu: IFirstLevelMenuItem[] = [
 ];
 
 export function Menu(): JSX.Element {
+  const pathname = usePathname();
   const { menu, setMenu, firstCategory } = useContext<IAppContext>(AppContext);
 
+  const openSecondCategory = (secondCategory: string) => {
+    setMenu &&
+      setMenu(
+        menu.map((m) => {
+          if (m._id.secondCategory === secondCategory) {
+            m.opened = !m.opened;
+          }
+          return m;
+        })
+      );
+  };
   const buildFirstLevelMenu = (): JSX.Element => {
     return (
       <>
         {firstLevelMenu.map((m) => {
           return (
             <div key={m.route}>
-              <a href={`/${m.route}`}>
+              <Link href={`/${m.route}`} style={{ textDecoration: "none" }}>
                 <div
                   className={cn(styles.firstLevel, {
                     [styles.firstLevelActive]: m.id === firstCategory,
@@ -59,7 +73,8 @@ export function Menu(): JSX.Element {
                   {m.icon}
                   <span>{m.name}</span>
                 </div>
-              </a>
+              </Link>
+
               {m.id === firstCategory && buildSecondLevelMenu(m.route)}
             </div>
           );
@@ -72,12 +87,27 @@ export function Menu(): JSX.Element {
     return (
       <div className={styles.secondBlock}>
         {menu.map((m) => {
+          if (
+            m.pages
+              .map((page) => {
+                return page.alias;
+              })
+              .includes(pathname.split("/")[2])
+          ) {
+            m.opened = true;
+          }
+
           return (
             <div key={m._id.secondCategory}>
-              <div className={styles.secondLevel}>{m._id.secondCategory}</div>
               <div
-                className={cn(styles.isOpened, {
-                  [styles.isOpenedActive]: m.opened,
+                className={styles.secondLevel}
+                onClick={() => openSecondCategory(m._id.secondCategory)}
+              >
+                {m._id.secondCategory}
+              </div>
+              <div
+                className={cn(styles.secondLevelOrdinary, {
+                  [styles.secondLevelActive]: m.opened,
                 })}
               >
                 {buildThirdLevelMenu(route, m.pages)}
@@ -92,15 +122,15 @@ export function Menu(): JSX.Element {
   const buildThirdLevelMenu = (route: string, pages: IPageItem[]) => {
     return pages.map((p) => {
       return (
-        <a
+        <Link
           href={`/${route}/${p.alias}`}
           key={p.alias}
           className={cn(styles.thirdLevel, {
-            [styles.thirdLevelActive]: true,
+            [styles.thirdLevelActive]: `/${route}/${p.alias}` === pathname,
           })}
         >
           {p.category}
-        </a>
+        </Link>
       );
     });
   };
--- layouts/sidebar/SideBar.tsx
+++ layouts/sidebar/SideBar.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import styles from "../layout.module.scss";
-import axios from "axios";
-import { MenuItem } from "../../interfaces/menu/menu.interfaces";
 import { Menu } from "../menu/Menu";
+
 async function SideBar() {
-  const firstCategory = 0;
-
-  const { data: menu } = await axios.post<MenuItem[]>(
-    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
-    {
-      firstCategory,
-    }
-  );
-
   return (
     <aside className={styles.sideBar}>
-      <Menu menuData={menu} />
+      <Menu />
     </aside>
   );
 }
